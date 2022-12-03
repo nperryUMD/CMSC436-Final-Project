013df165d3b54185a0b55226c8a1e161cb5942c7
Nicholas Perry <nperry1215@gmail.com>
2022-12-02 22:05:51 -0500

    Visual upgrades

--- app/src/main/java/com/example/composting/mainScroll/Datasource.kt
+++ app/src/main/java/com/example/composting/mainScroll/Datasource.kt
@@ -4,10 +4,9 @@ package com.example.composting.mainScroll
 class Datasource {
     fun load() : ArrayList<Data>{
         val dataList = ArrayList<Data>()
-        dataList.add(CompostCard(RecyclerViewAdapter.VIEW_TYPE_ONE, 1, 2, 3))
-        dataList.add(CalendarCard(RecyclerViewAdapter.VIEW_TYPE_THREE, "2. Hi! I am in View 2"))
-        dataList.add(GameCard(RecyclerViewAdapter.VIEW_TYPE_FOUR, 0,0))
-        dataList.add(AnalyticsCard(RecyclerViewAdapter.VIEW_TYPE_TWO, "4. Hi! I am in View 4"))
+        dataList.add(CompostCard(RecyclerViewAdapter.VIEW_COMPOST_BIN, 1, 2, 3))
+        dataList.add(GameCard(RecyclerViewAdapter.VIEW_GAME_BIN, 0,0))
+        dataList.add(AnalyticsCard(RecyclerViewAdapter.VIEW_ANALYTICS_BIN, "4. Hi! I am in View 4"))
         return dataList
     }
 
